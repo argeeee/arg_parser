42dbe9f5b6170e70819547d6262f77c28486f8e1
argeeee <argetimjusufi01@gmail.com>
2023-07-28 17:39:20 +0200

    feat: ✨ Added Option class

--- arg_parser/include/arg_parser/arg_parser.hpp
+++ arg_parser/include/arg_parser/arg_parser.hpp
@@ -1,3 +1,82 @@
 #pragma once
 
-int test();
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <functional>
+#include <iostream>
+
+class Option {
+ public:
+	Option(const std::string& name, char abbr = '\0', bool mandatory = false, std::vector<std::string> allowedValues = {})
+		: name(name), abbr(abbr), mandatory(mandatory), allowedValues(allowedValues)
+	{
+	}
+
+	// Add allowed value
+	void addAllowedValue(const std::string& value) {
+		allowedValues.push_back(value);
+	}
+
+	// Set default value
+	void setDefaultValue(const std::string& value) {
+		defaultValue = value;
+	}
+
+	// Set callback function
+	void setCallback(std::function<void(const std::string&)> callback) {
+		this->callback = std::move(callback);
+	}
+
+	// Getter functions
+	const std::string& getName() const {
+		return name;
+	}
+
+	char getAbbr() const {
+		return abbr;
+	}
+
+	bool isMandatory() const {
+		return mandatory;
+	}
+
+	const std::vector<std::string>& getAllowedValues() const {
+		return allowedValues;
+	}
+
+	const std::string& getDefaultValue() const {
+		return defaultValue;
+	}
+
+	const std::function<void(const std::string&)>& getCallback() const {
+		return callback;
+	}
+
+ private:
+	std::string name;
+	char abbr;
+	bool mandatory;
+	std::vector<std::string> allowedValues;
+	std::string defaultValue;
+	std::function<void(const std::string&)> callback;
+};
+
+class Flag {
+ public:
+	Flag(const std::string& name, char abbr = '\0', bool negatable = true)
+		: name(name), abbr(abbr), negatable(negatable)
+	{
+	}
+
+	// Set callback function
+	void setCallback(std::function<void()> callback) {
+		this->callback = std::move(callback);
+	}
+
+ private:
+	std::string name;
+	char abbr;
+	bool negatable;
+	std::function<void()> callback;
+};
--- arg_parser_tests/src/arg_parser.test.cpp
+++ arg_parser_tests/src/arg_parser.test.cpp
@@ -2,9 +2,52 @@
 
 #include "arg_parser/arg_parser.hpp"
 
-// Example test case
-TEST(ArgParserTest, testMethod) {
-	ASSERT_EQ(test(), 42);
+// Test the constructor and getter functions
+TEST(OptionTest, ConstructorAndGetter) {
+	Option option("name", 'a', true, {"value1", "value2"});
+	
+	EXPECT_EQ(option.getName(), "name");
+	EXPECT_EQ(option.getAbbr(), 'a');
+	EXPECT_TRUE(option.isMandatory());
+	EXPECT_EQ(option.getAllowedValues(), (std::vector<std::string>{"value1", "value2"}));
+}
+
+// Test adding allowed values
+TEST(OptionTest, AddAllowedValue) {
+	Option option("name");
+	option.addAllowedValue("value1");
+	option.addAllowedValue("value2");
+	
+	EXPECT_EQ(option.getAllowedValues(), (std::vector<std::string>{"value1", "value2"}));
+}
+
+// Test setting a default value
+TEST(OptionTest, SetDefaultValue) {
+	Option option("name");
+	option.setDefaultValue("default_value");
+	
+	EXPECT_EQ(option.getDefaultValue(), "default_value");
+}
+
+// Test setting a callback function
+TEST(OptionTest, SetCallback) {
+	Option option("name");
+	bool callbackCalled = false;
+	
+	option.setCallback([&callbackCalled](const std::string& value) {
+		callbackCalled = true;
+		EXPECT_EQ(value, "some_value"); // Check if the correct value is passed to the callback
+	});
+	
+	const auto& callbackFunction = option.getCallback();
+	ASSERT_TRUE(callbackFunction); // Ensure that the callback is set
+
+	// Simulate invoking the callback
+	if (callbackFunction) {
+		callbackFunction("some_value");
+	}
+
+	EXPECT_TRUE(callbackCalled);
 }
 
 int main(int argc, char **argv) {
